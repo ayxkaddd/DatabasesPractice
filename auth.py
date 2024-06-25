a0e57d3e7340ced8be17b49ed6a6235339fe8b0e
x <bladeec1ty@protonmail.com>
2024-06-25 15:37:53 +0300

    change JWT token exp to 1 day

--- auth.py
+++ auth.py
@@ -19,7 +19,7 @@ class AuthHandler():
 
     def encode_token(self, user_id):
         payload = {
-            'exp': datetime.utcnow() + timedelta(days=0, minutes=30),
+            'exp': datetime.utcnow() + timedelta(days=1),
             'iat': datetime.utcnow(),
             'sub': user_id
         }
